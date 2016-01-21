4f74c5cdf82b786cab360d661be044499f29056f
Amy Zumbahlen <azumb3@gmail.com>
2016-01-21 16:32:45 -0600

    First commit before testing

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 
+## Functions utilizing caching to calculate Matrix inversions
+##
 
-## Write a short comment describing this function
+## makeCacheMatrix
+## This function creates a matrix "object" used to cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+  minv <- NULL
+  set <- function(y) {
+    x <<- y
+    minv <<- NULL
+  }
+  get <- function() x
+  inv_set <- function(solve) minv <<- solve
+  inv_get <- function(solve) minv
+  list(set = set, get = get,
+       inv_set = inv_set,
+       inv_get = inv_get)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve
+## This function computes the inversion of a square matrix based on matrix created in makeCacheMatrix 
+## 
+##    If inverse has been calculated and value has NOT changed
+##       Use cached matrix and display notification message
+##    else
+##       Calculate new inversion
 
 cacheSolve <- function(x, ...) {
+  minv <- x$inv_get()
+  if(!is.null(minv)) {
+    message("getting cached data")
+    return(minv)
+  }
+  matrix <- x$get()
+  minv <- solve(matrix, ...)
+  x$inv_set(minv)
+  return(minv)
+}
